47c3e7ef22d593c87511630bd165361f94a57d0b
Hooverdan96 <35113775+Hooverdan96@users.noreply.github.com>
2022-03-02 17:28:57 -0800

    Update run_kiwi.sh with changes for boxbuild
    Enable boxbuild for kiwi-ng
    place relevant git inside vagrant box to avoid nested mapped drive issues for qemu/KVM
    force copy rockstor.kiwi file into git, so user's edits (kernel backport, different profiles, etc.) are taken into account
    after image generation force copy entire directory back to vagrant host's directory including logs
    cleanup git and images directory on vagrant box

--- vagrant_env/run_kiwi.sh
+++ vagrant_env/run_kiwi.sh
@@ -1,23 +1,63 @@
 #!/bin/bash
+# Bash parameters
 set -e
 set -u
 #set -x
 
-PROFILE=${1:-x86_64}
-#PROFILE="RaspberryPi4"
+# Processing Profile Parameters
+PROFILE="Leap15.3.x86_64"
 
-KIWI_BUILD_DIR="/home/vagrant/kiwi-images/"
-REPO_DIR="/home/vagrant/rockstor-installer/"
+# Memory and CPUs need to be less than stipulated in the Vagrantfile
+MEM="6G"
+CPU="3"
+
+# Set Path Variables
+HOME_PATH="/home/vagrant"
+REPO_SOURCE_PATH="/home/vagrant/rockstor-installer"
+REPO_PATH="/home/vagrant/repo"
+REPO_URL="https://github.com/rockstor/rockstor-installer.git"
+KIWI_NG_PATH="/home/vagrant/kiwi-env/bin"
+KIWI_IMAGES="/home/vagrant/kiwi-images/"
+DESCRIPTION="./"
 
 echo '============================================='
 echo 'Starting Build'
 echo '============================================='
-cd ${REPO_DIR}
-if [ -e ${KIWI_BUILD_DIR} ]; then
-    sudo rm -rf "${KIWI_BUILD_DIR}/build";
+echo 'Get most recent git into Vagrant Box'
+echo '============================================='
+
+if [ ! -e ${REPO_PATH} ]; then
+	sudo git clone ${REPO_URL} ${REPO_PATH}
 fi
-sudo kiwi-ng --profile=Leap15.2."${PROFILE}" --type oem system build --description ./ --target-dir "${KIWI_BUILD_DIR}/"
-sudo find ${KIWI_BUILD_DIR} -name "*.iso" -exec cp {} /vagrant/ \;
+
+echo '============================================='
+echo 'insert rockstor.kiwi from local directory'
+echo '============================================='
+# force copy to have the latest version inside the vagrant box
+sudo cp -fv "$REPO_SOURCE_PATH"/rockstor.kiwi "$REPO_PATH"/rockstor.kiwi
+
+echo '============================================='
+echo 'executing kiwi-ng'
+echo '============================================='
+# change into repo directory before executing kiwi-ng
+cd "$REPO_PATH"
+sudo "$KIWI_NG_PATH"/kiwi-ng --profile="$PROFILE" --type oem system boxbuild --box-memory "$MEM" --box-smp-cpus="$CPU" --box leap -- --description "$DESCRIPTION" --target-dir "$KIWI_IMAGES"
+
+echo '============================================='
+echo 'Copying image directory to local'
+echo '============================================='
+# force copy to outside of vagrant box
+sudo cp -fRv "$KIWI_IMAGES" "$REPO_SOURCE_PATH"
+
+echo '============================================='
+echo 'Clean up kiwi-images and git repo'
+echo '============================================='
+cd "$HOME_PATH"
+sudo rm -rf "$KIWI_IMAGES"
+sudo rm -rf "$REPO_PATH"
 echo '============================================='
 echo 'Finished Build'
+echo '============================================='
+echo 'Status and result logs can be found in "$REPO_SOURCE_PATH" directory'
+echo 'Auf Wiedersehen'
 echo '============================================='
