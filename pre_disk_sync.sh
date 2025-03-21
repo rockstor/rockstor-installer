4175b373185c94bc68c758c0cd418dc6e337cf27
Philip Guyton <philip@yewtreeapps.com>
2025-03-21 16:22:04 +0000

    Add SELinux tools, selinux config re config.bootoptions #200

--- pre_disk_sync.sh
+++ pre_disk_sync.sh
@@ -11,17 +11,25 @@
 selinuxcmdline=('security=selinux' 'selinux=1')
 # SELinux kernel options added to rockstor.kiwi image.preferences.type kernelcmdline= entries.
 if [[ -e /etc/selinux/config ]]; then
+    # SELinux config
+    sed -i -e 's|^SELINUX=.*|SELINUX=permissive|g' \
+           -e 's|^SELINUXTYPE=.*|SELINUXTYPE=targeted|g' \
+           "/etc/selinux/config"
+    echo "-- /etc/selinux/config updated -----"
     # Grub kernel cmdline additions
     if [[ -e /etc/default/grub ]]; then
         sed -i "s|^GRUB_CMDLINE_LINUX_DEFAULT=\"|&${selinuxcmdline[*]} |" /etc/default/grub
+        echo "-- /etc/default/grub updated -------"
     fi
     # Sysconfig bootloader cmdline additions (installer boot)
     if [[ -e /etc/sysconfig/bootloader ]]; then
         sed -i "s|^DEFAULT_APPEND=\"|&${selinuxcmdline[*]} |" /etc/sysconfig/bootloader
         sed -i "s|^FAILSAFE_APPEND=\"|&${selinuxcmdline[*]} |" /etc/sysconfig/bootloader
+        echo "-- /etc/sysconfig/bootloader updated"
     fi
-    # SELinux config
-    sed -i -e 's|^SELINUX=.*|SELINUX=permissive|g' \
-           -e 's|^SELINUXTYPE=.*|SELINUXTYPE=targeted|g' \
-           "/etc/selinux/config"
+    # Update kiwi-ng's bootoptions config file used by dracut
+    if [[ -e /config.bootoptions ]]; then
+        sed -i "1 s|.|${selinuxcmdline[*]} &|" /config.bootoptions
+        echo "-- /config.bootoptions updated -----"
+    fi
 fi
